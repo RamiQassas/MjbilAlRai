fcb4055bea57133a2db8cacc140aa47f157bd7ba
RamiQassas <a0947991951@hotmail.com>
2024-10-11 23:34:42 +0300

    Update settings.py

--- MjbilAlRai/settings.py
+++ MjbilAlRai/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mu4gjeevzqedt%1spgl!1=qr8(ebi&1$94-^@f+=k5mxlv0edb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mjbilalrai.onrender.com']
 
 
 # Application definition
