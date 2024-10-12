ec275dbd94d5c4171f788fa55cf7e85b8956bd51
RamiQassas <a0947991951@hotmail.com>
2024-10-12 15:17:18 +0300

    Update settings.py

--- MjbilAlRai/settings.py
+++ MjbilAlRai/settings.py
@@ -79,16 +79,18 @@ WSGI_APPLICATION = 'MjbilAlRai.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mjbilalrai_db',
-        'USER': 'mjbiluser',
-        'PASSWORD': '@@Kaito1412@@',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'migbilrahi'),
+        'USER': os.getenv('DB_USER', 'migbilrahi'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '8dzSXv0QwuQdXpoEISAY6IQWNi0qojrf'),
+        'HOST': os.getenv('DB_HOST', 'dpg-cs5681g8fa8c73agka70-a.frankfurt-postgres.render.com'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 
